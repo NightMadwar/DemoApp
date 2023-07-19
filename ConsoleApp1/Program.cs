feb4e6d5240c18b03a82c05ff27d45611d77c86b
Madwar <Madwar@DESKTOP-V4JAS17>
2023-07-19 10:42:50 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using static Math.Math;
+using IUST;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 5;
+        x = int.Parse(Console.ReadLine()!);
+       
+        x = Increament(x);
+        Console.WriteLine($"x is {x}");
+        Student student = new();
+    }
+}
---
+++ Math/Math.cs
@@ -0,0 +1,11 @@
+﻿namespace Math
+{
+    public class Math
+    {
+        public static int Increament(int Num)
+        {
+            Num = 10;
+            return Num;
+        }
+    }
+}
