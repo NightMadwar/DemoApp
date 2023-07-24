4fc77306104797db0fe74ee2820963003014bc5f
Maher Madwar <mahermadwar2001@hotmail.com>
2023-07-24 12:17:19 +0100

    improvement of filtering

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,27 +10,34 @@ internal class Program
         //Console.WriteLine("Enter a Num");
         //x = int.Parse(Console.ReadLine()!);
         //int y;
-        //Increment(x,out y);           
+        //Increment(x, out y);
         //Console.WriteLine($"x is {x} ,y is {y}");
-        //Console.ReadKey(); 
-        //List<int> arr = new List<int> {1,2,3,4,5};
-        //List<string> arr2 = new List<string> {"Maher", "Yousef"};
+        //Console.ReadKey();
+        //List<int> arr = new List<int> { 1, 2, 3, 4, 5 };
+        //List<string> arr2 = new List<string> { "Maher", "Yousef" };
         //arr.Print();
         //Console.WriteLine();
-        //arr2.Print();
-        List<Employee> employees = new List<Employee>() 
+        //arr2.Filtering(true);
+
+        List<Employee> employees = new List<Employee>()
         {
             new Employee{Id = 1, Name ="Maher",Description="professor",IsManager=true},
             new Employee{Id = 2, Name ="Yousef",Description="Student",IsManager=true},
             new Employee{Id = 3, Name ="Wissam",Description="dancer",IsManager=false},
-            new Employee{Id = 4, Name ="Adnan",Description="doctor",IsManager=false}
+            new Employee{Id = 4, Name ="Adnan",Description="doctor",IsManager=false},
+            new Employee{Id = 5, Name ="Adnan",Description="doctor",IsManager=false},
+            new Employee{Id = 6, Name ="Adnan",Description="doctor",IsManager=false},
         };
-        List<Employee> FilteredEmp = employees.Filtering(a => a.IsManager == true);
-        foreach(Employee emp in FilteredEmp)
-        {
-            emp.SetInfo();
-            Console.WriteLine();
-        }
+        //List<Employee> FilteredEmp = employees.Filtering(a => a.IsManager == true);
+        //foreach (Employee emp in FilteredEmp)
+        //{
+        //    emp.SetInfo();
+        //    Console.WriteLine();
+        //}
+        employees.Filtering(e => e.IsManager == true).Print();
+
+        List<Employee> filtered = employees.Filtering(e =>e.IsManager == false);
+        filtered.Print();
     }
     
 }
@@ -84,5 +91,22 @@ public static class Filter
         }
         return arr;
 
+    }
+    public static void Print<T>(this List<T> records)
+    {
+        if (typeof(T) == typeof(Employee))
+        {
+            foreach( T record in records)
+            {
+                ((Employee) (object)record).SetInfo();
+            }
+        }
+        else
+        {
+            foreach (T record in records)
+            {
+                Console.WriteLine(record);
+            }
+        }
     }
 }
